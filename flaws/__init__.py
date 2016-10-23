1239128a082757c3a7d53e7b14c189dda06f4171
Alexander Schepanovski <suor.web@gmail.com>
2016-10-23 16:54:24 +0700

    Make ipdb hook turn on only when --ipdb

--- flaws/__init__.py
+++ flaws/__init__.py
@@ -6,24 +6,27 @@ from funcy import split, map
 from .analysis import global_usage, local_usage, FileSet
 
 
-import sys, ipdb, traceback
-
-def info(type, value, tb):
-    traceback.print_exception(type, value, tb)
-    print
-    ipdb.pm()
-
-sys.excepthook = info
-
-
 def main():
     command = sys.argv[1]
     kwargs, args = split(r'^--', sys.argv[2:])
-    kwargs = dict(map(r'^--(\w+)=(.+)', kwargs))
+    kwargs = dict(map(r'^--(\w+)(?:=(.+))?', kwargs))
 
+    # Run ipdb on exception
+    if 'ipdb' in kwargs:
+        import ipdb, traceback
+
+        def info(type, value, tb):
+            traceback.print_exception(type, value, tb)
+            print
+            ipdb.pm()
+
+        sys.excepthook = info
+
+    # Register plugins
     from .ext import django
     django.register(args, kwargs)
 
+    # Do the job
     files = FileSet(args, base=kwargs.get('base'), ignore=kwargs.get('ignore'))
     if command == 'global':
         global_usage(files)
